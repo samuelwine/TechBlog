8e1f0743c29df8a098617d840b25fc0225394572
Shmuel <samuelwine@gmail.com>
2021-12-16 01:41:09 +0000

    Add Statiq Bootstrapper

--- StatiqConsoleApp/Program.cs
+++ StatiqConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Statiq.App;
+using Statiq.Web;
+
+namespace MySite
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+          await Bootstrapper
+            .Factory
+            .CreateDefaultWithout(args, DefaultFeatures.Pipelines)
+            .AddWeb()
+            .RunAsync();
+    }
+}
